787cd13bb5ccfc78ba26be2eb064c47cb8effe63
Jack Saunders <jack@oldstlabs.com>
2014-05-21 04:24:20 -0700

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flask_servatus",
-    version="0.0.2",
+    version="0.0.3",
     author='Old Street Labs',
     author_email='developers@oldstlabs.com',
     packages=find_packages(exclude=[])
