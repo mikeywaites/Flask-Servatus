608d98674dec6c901dfea08aa469a59e41319676
Mike Waites <mikey.waites@gmail.com>
2015-02-08 22:49:26 +0000

    cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from setuptools import setup, find_packages
-
-setup(
-    name="flask_servatus",
-    version="0.0.3",
-    author='Mikey Waites',
-    author_email='mikey.wait.es@gmail.com',
-    packages=find_packages(exclude=[])
-)
-
-
 import os
 import sys
 
